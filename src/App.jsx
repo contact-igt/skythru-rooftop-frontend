902d17dd19f7b2f20a494a11f8cd90ce7d07f08f
tariq <tariq@salina.group>
2026-02-03 13:55:40 +0530

    added gtm code

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import ThankYou from './components/ThankYou';
+import GTMPageView from './components/hooks/useUTMSource/gtmpageview';
 
 export default function App() {
     return (
         <Router>
+            <GTMPageView/>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/thank-you" element={<ThankYou />} />
